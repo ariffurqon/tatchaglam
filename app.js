74a6db5f1ec317ceac4797f6a16ce7dd8d3805c0
ariffurqon <arif.furqon@gmail.com>
2016-02-15 03:33:12 -0800

    added routing and templates

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var app = angular.module('tatchaTouch', ['ngRoute', 'ngAnimate']);
+	app.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+		    .when('/', {
+		      templateUrl: 'templates/main.html',
+		      controller: 'MainCtrl'
+		    })
+
+		 	.otherwise({
+		 	    redirectTo: '/'
+		 	});
+		 	
+		}]);
 
 	app.controller('MainCtrl', ['$scope', '$http',function($scope, $http){
 		$scope.photos = [];
